6b705cd934032a4b320ef08bf44f2fda51373083
ArtemShkurenko <artnorm@ukr.net>
2025-02-20 00:15:39 +0200

    my work

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime endDate = startDate.AddDays(dayCount - 1);
+            if (weekEnds == null || weekEnds.Length == 0)
+            {
+                return endDate;
+            }
+           
+            foreach (WeekEnd weekEnd in weekEnds)
+            {
+                if(weekEnd.StartDate <= endDate && weekEnd.StartDate >= startDate)
+                {
+                    if(weekEnd.EndDate <= endDate)
+                    {
+                        int extraDays = (weekEnd.EndDate - weekEnd.StartDate).Days + 1;
+                        endDate = endDate.AddDays(extraDays);
+                    }
+                    else 
+                    {
+                        int extraDays = (weekEnd.EndDate - endDate).Days  + 1;
+                        endDate = endDate.AddDays(extraDays);
+                    }                   
+                }             
+            }           
+            return endDate;
+        }
+    }
+}
